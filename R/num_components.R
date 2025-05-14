4eca21bec256a71a9af5095c202e7c9662d687ce
Eric V. Strobl <ericvonstrobl@gmail.com>
2025-05-13 21:32:46 -0400

    Add files via upload

---
+++ R/num_components.R
@@ -0,0 +1,12 @@
+num_components <- function(Tx,Y,ncs=2:5){
+
+  vars = c()
+  for (n in ncs){
+    mod = SV(Tx,Y1,nc=n)
+    values = permutation_testing(mod,Tx,Y,1000,nc=n)$omnibus$pval
+    vars = c(vars,values)
+  } 
+  
+  return(ncs[which(vars==min(vars))[1]])
+
+}
